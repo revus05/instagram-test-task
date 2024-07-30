9d7d09700522a71fce562a16c5859b56bee83a86
revus05 <skninni05@gmail.com>
2024-07-30 13:58:23 +0300

    chore: config tailwind and typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				roboto: ['Roboto', 'Arial', 'sans-serif'],
+				// Здесь 'sans' - это имя шрифта по умолчанию для вашего проекта, можете добавить свои
+			},
+			colors: {
+				primary: '#4991f3',
+				'primary-active': '#1877f2',
+				'secondary-btn': '#efefef',
+				'secondary-btn-active': '#dbdbdb',
+				secondary: '#8E8E8E',
+			},
+		},
 	},
 	plugins: [],
 }
