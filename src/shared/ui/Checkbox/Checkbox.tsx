11c3d5e8f8e36f2f1290805f50160104173af42c
revus05 <skninni05@gmail.com>
2024-07-30 15:55:18 +0300

    feat(ui/Checkbox): create Checkbox.tsx

---
+++ src/shared/ui/Checkbox/Checkbox.tsx
@@ -0,0 +1,24 @@
+import { InputHTMLAttributes, useId } from 'react'
+import { twMerge } from 'tailwind-merge'
+import checkedSVG from '../../../../public/checked.svg'
+
+type Props = InputHTMLAttributes<HTMLInputElement> & {
+	label: string
+}
+
+const Checkbox = ({ label, className, checked, ...props }: Props) => {
+	const labelId = useId()
+	return (
+		<label htmlFor={labelId} className={'flex items-center cursor-pointer'}>
+			<div className={'w-4'}>
+				<input id={labelId} type={'checkbox'} className={'opacity-0 absolute'} {...props} />
+				<span className={twMerge('flex h-4 w-4 rounded-[3px] border items-center justify-center', className)}>
+					{checked ? <img src={checkedSVG} alt="checked" className={'h-3 w-3'} /> : null}
+				</span>
+			</div>
+			<p className={'grow text-sm leading-4 font-semibold ml-2'}>{label}</p>
+		</label>
+	)
+}
+
+export default Checkbox
